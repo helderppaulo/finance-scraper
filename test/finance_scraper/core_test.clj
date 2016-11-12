3d8db8d8713ccfa5161c52cdcb0976bb1b263296
helderppaulo <helderppaulo89@gmail.com>
2016-11-12 15:40:45 -0200

    first commit

---
+++ test/finance_scraper/core_test.clj
@@ -0,0 +1,7 @@
+(ns finance-scraper.core-test
+  (:require [clojure.test :refer :all]
+            [finance-scraper.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
