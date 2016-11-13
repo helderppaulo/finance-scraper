ae753aefb29c28ec261c324357788e96b4b56e5f
Eric Liou <ercliou@gmail.com>
2016-11-13 02:30:52 -0200

    Extract to functions

--- src/finance_scraper/core.clj
+++ src/finance_scraper/core.clj
@@ -1,22 +1,37 @@
 (ns finance-scraper.core
-  (require [clj-http.client :as client]
+  (require [clj-http.client :as http]
            [hickory.select :as s]
            [hickory.core :as parser]))
 
-(defn scrape-di []
-  (def raw-site (:body (client/get "https://www.cetip.com.br/")))
-  (def site-htree (parser/as-hickory (parser/parse raw-site)))
-  (-> (s/select (s/id :ctl00_Banner_lblTaxDI) site-htree)
-      first :content first
+(def site-di "https://www.cetip.com.br/")
+
+(defn parse-htree [raw]
+  (parser/as-hickory (parser/parse raw)))
+
+(defn select-di [htree]
+  (->> htree
+       (s/select (s/id :ctl00_Banner_lblTaxDI))
+       first :content first))
+
+(defn pct-str->number [di-str]
+  (-> di-str
       (clojure.string/replace #"%" "")
       (clojure.string/replace #"," ".")
-      (read-string))
-  )
+      read-string))
 
+(defn extract-di [raw-site]
+  (-> raw-site
+      parse-htree
+      select-di
+      pct-str->number))
+
+(defn scrape-di []
+  (-> (http/get site-di)
+      :body
+      extract-di))
 
 (defn -main
   [& args]
   (println "scraping taxa DI")
-  (clojure.pprint/pprint (scrape-di))
-  )
+  (clojure.pprint/pprint (scrape-di)))
 
