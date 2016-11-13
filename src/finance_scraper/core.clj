f479b97fc581a363bf5206faea7f6e176e4ac380
helderppaulo <helderppaulo89@gmail.com>
2016-11-13 01:33:10 -0200

    scraping DI rate

--- src/finance_scraper/core.clj
+++ src/finance_scraper/core.clj
@@ -1,7 +1,22 @@
 (ns finance-scraper.core
-  (:gen-class))
+  (require [clj-http.client :as client]
+           [hickory.select :as s]
+           [hickory.core :as parser]))
+
+(defn scrape-di []
+  (def raw-site (:body (client/get "https://www.cetip.com.br/")))
+  (def site-htree (parser/as-hickory (parser/parse raw-site)))
+  (-> (s/select (s/id :ctl00_Banner_lblTaxDI) site-htree)
+      first :content first
+      (clojure.string/replace #"%" "")
+      (clojure.string/replace #"," ".")
+      (read-string))
+  )
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "scraping taxa DI")
+  (clojure.pprint/pprint (scrape-di))
+  )
+
